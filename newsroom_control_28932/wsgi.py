d3c686b6c3777f5fa2f75928e0f651327073aea4
Crowdbotics <team@crowdbotics.com>
2021-07-18 19:37:20 +0000

    Update models

--- newsroom_control_28932/wsgi.py
+++ newsroom_control_28932/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsroom_control_28932.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsroom_control_28932.settings")
 
 application = get_wsgi_application()
